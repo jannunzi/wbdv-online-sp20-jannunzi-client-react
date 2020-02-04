cc8f41c282be4afe313fd01e2077bdf4671dfe53
jannunzi <jannunzi@gmail.com>
2020-02-03 20:57:38 -0500

    navigating from one page to the next also survives browser reload back and forward buttons fully functional work as expected

--- src/components/Page2.js
+++ src/components/Page2.js
@@ -1,6 +1,12 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 const Page2 = () =>
-    <h1>Page 2</h1>
+    <div>
+        <h1>Page 2</h1>
+        <Link to="/page1">
+            Page 1
+        </Link>
+    </div>
 
 export default Page2
